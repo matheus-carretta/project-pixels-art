ee400b33d08f8358c97cb470c2fe6701a1681add
Matheus Carretta <matheuscarretta@gmail.com>
2021-03-09 17:11:04 -0300

    Pega a class selected conforme paleta que for clicada

--- script.js
+++ script.js
@@ -10,3 +10,39 @@ for (let pixelRows = 0; pixelRows < numberPixels; pixelRows += 1) {
     document.querySelectorAll('.pixel-row')[pixelRows].appendChild(pixel);
   }
 }
+
+let firstPalette = document.getElementById('black');
+let secondPalette = document.getElementById('blue');
+let thirdPalette = document.getElementById('green');
+let fourthPalette = document.getElementById('yellow');
+
+// Achei a função neste site: https://www.w3schools.com/howto/howto_js_remove_class.asp
+function removeSelectedClass() {
+  let selectedColor = document.querySelector('.selected');
+  selectedColor.classList.remove('selected');
+}
+
+function getFirstPaletteColor() {
+  removeSelectedClass();
+  firstPalette.className += firstPalette.className = ' selected';
+}
+
+function getSecondPaletteColor(){
+  removeSelectedClass();
+  secondPalette.className += secondPalette.className = ' selected';
+}
+
+function getThirdPaletteColor(){
+  removeSelectedClass();
+  thirdPalette.className += thirdPalette.className = ' selected';
+}
+
+function getFourthPaletteColor(){
+  removeSelectedClass();
+  fourthPalette.className += fourthPalette.className = ' selected';
+}
+
+firstPalette.addEventListener('click', getFirstPaletteColor);
+secondPalette.addEventListener('click', getSecondPaletteColor);
+thirdPalette.addEventListener('click', getThirdPaletteColor);
+fourthPalette.addEventListener('click', getFourthPaletteColor);
