d70c16c6ed804f2f4099ee1e30f70bbe04fffbbf
Matheus Carretta <matheuscarretta@gmail.com>
2021-03-11 14:48:53 -0300

    Coloca o codigo de criar o board dentro de uma funcao

--- script.js
+++ script.js
@@ -4,16 +4,19 @@ window.onload = function (){
 
 let numberPixels = 5;
 
-for (let pixelRows = 0; pixelRows < numberPixels; pixelRows += 1) {
-  let pixelRow = document.createElement('div');
-  pixelRow.className = 'pixel-row';
-  document.getElementById('pixel-board').appendChild(pixelRow);
-  for (let pixelColumns = 0; pixelColumns < numberPixels; pixelColumns += 1) {
-    let pixel = document.createElement('div');
-    pixel.className = 'pixel';
-    document.querySelectorAll('.pixel-row')[pixelRows].appendChild(pixel);
+function createBoard() {
+  for (let pixelRows = 0; pixelRows < numberPixels; pixelRows += 1) {
+    let pixelRow = document.createElement('div');
+    pixelRow.className = 'pixel-row';
+    document.getElementById('pixel-board').appendChild(pixelRow);
+    for (let pixelColumns = 0; pixelColumns < numberPixels; pixelColumns += 1) {
+      let pixel = document.createElement('div');
+      pixel.className = 'pixel';
+      document.querySelectorAll('.pixel-row')[pixelRows].appendChild(pixel);
+    }
   }
 }
+createBoard ();
 
 let firstPalette = document.getElementById('black');
 let secondPalette = document.getElementById('blue');
